0b94fb9a89e0bf5a6d10603f3b72e96b78ae9bdc
OzPol <95313700+OzPol@users.noreply.github.com>
2023-06-08 12:28:57 -0400

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,12 @@
+
+user_name = input("Hello. What is your name? ")
+print("It's nice to meet you, " + user_name + ". How old are you?", end=" ")
+# end=" " at the end of the line prompts the user to enter their input on the same line as the sentence instead of new line characters. 
+
+#take user's age as input.
+user_age = input()
+print("I see that you are still quite young at only " + user_age + ".", end=" ")
+
+# take user's residence as input
+residence = input("Where do you live? ")
+print("Wow! I've always wanted to go to " + residence + ". Thanks for chatting with me. Bye!")
